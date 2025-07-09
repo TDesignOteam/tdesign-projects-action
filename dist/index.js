6eaea517df7071306910aa2df84435ae78d9b27c
阿菜 Cai <jimmyrss1102@gmail.com>
2025-07-10 02:23:07 +0800

    chore: 更新构建后的文件

--- dist/index.js
+++ dist/index.js
@@ -31411,7 +31411,8 @@ const issue2Projects = async (octokit) => {
         coreExports.error('未找到 deviceFieldId');
         return;
     }
-    const DeviceOptionId = queryFieldsSingleSelectOptionId(deviceField.options, repoFields[repo].Device);
+    coreExports.info(`Device select id: ${repoFields[repo].Device}`);
+    const DeviceOptionId = await queryFieldsSingleSelectOptionId(deviceField.options, repoFields[repo].Device);
     // 更新多个字段
     const updates = [
         {
